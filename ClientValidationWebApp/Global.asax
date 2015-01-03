d87a8f6f8e721811bf022fc50bc47f368d140691
Prashant Kurlekar <prashantkurlekar@gmail.com>
2015-01-04 00:05:17 +1100

    Initial commit

---
+++ ClientValidationWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientValidationWebApp.MvcApplication" Language="C#" %>
---
+++ ClientValidationWebApp/Models/EmployeeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientValidationWebApp.Models
+{
+    public class EmployeeViewModel
+    {
+        //Basic details
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        //Address
+        [Required]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public string PostCode { get; set; }
+
+        //Other Details
+        [Required]
+        public DateTime DOB { get; set; }
+        public string Other { get; set; }
+    }
+}
